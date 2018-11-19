29ae8df14b9aaff302467ab9305796023417afbd
Gabriel Soares <gabrielsxp@hotmail.com>
2018-11-19 15:41:28 -0200

    Updates

---
+++ precache-manifest.fe3644b7bfd40105018df7350683356c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cede4cfcb84b0e68db7d",
+    "url": "/react-currency-history/static/js/runtime~main.cede4cfc.js"
+  },
+  {
+    "revision": "02c13a9a9f3c387e21cc",
+    "url": "/react-currency-history/static/js/main.02c13a9a.chunk.js"
+  },
+  {
+    "revision": "1164749ae45ed4df8844",
+    "url": "/react-currency-history/static/js/1.1164749a.chunk.js"
+  },
+  {
+    "revision": "02c13a9a9f3c387e21cc",
+    "url": "/react-currency-history/static/css/main.f8329845.chunk.css"
+  },
+  {
+    "revision": "67c5c44a6fd2dd00aaa48a94783cb992",
+    "url": "/react-currency-history/index.html"
+  }
+];
